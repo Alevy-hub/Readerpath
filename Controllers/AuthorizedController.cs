b0f84267b9a4734b3d850944896126b1f658fb99
Alevy <rafalkujawa16@gmail.com>
2023-06-10 12:40:58 +0200

    When nothing in searchbar result is every book in db. When no books read in month its block in statistics has other color

--- Controllers/AuthorizedController.cs
+++ Controllers/AuthorizedController.cs
@@ -66,7 +66,7 @@ namespace Readerpath.Controllers
         {
             if(query == null)
             {
-                return RedirectToAction("LoggedIndex");
+                query = "";
             }
             using (var context = new ApplicationDbContext(_options))
             {
